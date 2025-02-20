9c054fe328f2e25193280f52eb268c973dcdf817
cristivalea <160530571+cristivalea@users.noreply.github.com>
2025-02-20 20:32:06 +0200

    Modificare clasa Student

---
+++ model/Main.py
@@ -0,0 +1,10 @@
+def inscriere_facultate(data_start, data_end):
+    '''
+    :param data_start:
+    :param data_end:
+    :return: returneaza o lista de studenti
+    pentru fiecare din zilele scurse dintre cele doua date se genereaza aleator numarul de studenti inscrisi in fiecare zi (100, 150)
+    pentru o zi jumatate sunt fete si jumatate baieti
+    baietii si fetele se adauga in lista
+    '''
+    pass
