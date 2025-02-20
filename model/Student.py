c000d4533909f5d52d2c0a6c886c648cad3106c2
cristivalea <160530571+cristivalea@users.noreply.github.com>
2025-02-20 20:08:26 +0200

    Terminare clasa Persoana, Creare clasa Student si fisier de cai

---
+++ model/Student.py
@@ -0,0 +1,57 @@
+import random
+from datetime import date
+from datetime import timedelta
+
+from model.Persoana import Persoana
+
+
+class Student(Persoana):
+    def __init__(self, sex, varsta):
+        super().__init__(sex, varsta)
+        self.__nr_matricol = self.generare_numar_matricol()
+        self.__data_inmatricluare = self.data_inmatriculare(date(2020, 1, 1), date(2020, 1, 7))
+        self.__cale_poze = self.citire_cai_fisier("cai_imagini.txt")
+        #print(self.__cale_poze)
+
+
+    def generare_numar_matricol(self):
+        nr_matricol = "CTI0"
+        data_curenta = date.today()
+        an_curent = data_curenta.year
+        nr_matricol += str(an_curent % 100)
+        numar = random.randint(100, 1000)
+        coduri_nr_matricole = []
+        if numar not in coduri_nr_matricole:
+            coduri_nr_matricole.append(numar)
+            nr_matricol += str(numar)
+            #
+        else:
+            self.generare_numar_matricol()
+        return nr_matricol
+    def get_nr_matricol(self):
+        return self.__nr_matricol
+
+    def data_inmatriculare(self, data_start, data_end):
+        diferenta_zile = (data_end - data_start).days
+        zile_aleatoare = random.randint(0, diferenta_zile)
+        data_inmatriculare = data_start + timedelta(days=zile_aleatoare)
+        return data_inmatriculare
+
+    def get_data_inmatriculare(self):
+        return self.data_inmatriculare
+
+    def citire_cai_fisier(self, file):
+        f = open(file, "r", encoding="utf-8")
+        if f == None:
+            raise Exception("Nu s-a deschis fisierul")
+        cai = []
+        for line in f:
+            cai.append(line)
+        return cai
+    def __repr__(self):
+        persoana_info = super().__repr__()
+        return f"{persoana_info}, Numar matricol: {self.__nr_matricol}, Data inmatriculare: {self.__data_inmatricluare}"
+
+
+student1 = Student('M', 20)
+print(student1)
